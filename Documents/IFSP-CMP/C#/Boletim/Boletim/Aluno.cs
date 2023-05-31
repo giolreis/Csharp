edaa8a8ca4f3f9fe62ca48ac46d63450424aff26
giolreis <giovaniluizreis@gmail.com>
2023-05-31 20:33:00 -0300

    first commit

---
+++ Documents/IFSP-CMP/C#/Boletim/Boletim/Aluno.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boletim
+{
+    internal class Aluno
+    {
+
+        public string nome;
+        public string curso;
+        public double nota1;
+        public double nota2;
+        public double media;
+
+
+        public double Media()
+        {
+            media = (nota1 + nota2) / 2;
+            return media; 
+
+        }
+
+        public string situacao()
+        {
+            if (media <=5 && media >=4)
+            {
+                return "IFA";
+            }
+            else if (media <4) {
+                return "Reprovado";
+            }
+            else
+            {
+                return "Aprovado";
+            }
+        }
+
+
+
+    } 
+}
---
+++ Documents/IFSP-CMP/C#/Boletim/Boletim/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boletim
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno x = new Aluno();
+
+            Console.Write("Digite seu nome: ");
+            x.nome= Console.ReadLine();
+            Console.Write("Digite seu curso: ");
+            x.curso = Console.ReadLine();
+            Console.Write("Digite a primeira nota: ");
+            x.nota1 = double.Parse(Console.ReadLine());
+            Console.Write("Digite a segunda nota: ");
+            x.nota2 = double.Parse(Console.ReadLine());
+            Console.Clear();
+            Console.WriteLine("\n\nBoletim do aluno !!! \n");
+            Console.WriteLine("" + x.nome);
+            Console.WriteLine("" + x.curso);
+            Console.WriteLine("Nota Final:{0} ",x.Media() );
+            Console.WriteLine("Situação Final: {0} \n", x.situacao());      
+         
+
+
+
+        }
+    }
+}
